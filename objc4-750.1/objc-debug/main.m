b4fd8540038be2abc48ae0b7650961b6c417195e
Jashion <ycyz138258886@qq.com>
2019-07-22 18:10:28 +0800

    添加可运行的iOS Runtime运行环境的所有文件。

---
+++ objc4-750.1/objc-debug/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  objc-debug
+//
+//  Created by Jashion on 2019/7/22.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSObject *obj = [NSObject new];
+    }
+    return 0;
+}
